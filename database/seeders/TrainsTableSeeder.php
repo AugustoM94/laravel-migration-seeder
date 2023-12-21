46049efad16df5fe544b110341b563768e15c886
AugustoM94 <email@github.com>
2023-12-21 16:21:20 +0100

    Added model Train

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Faker\Generator as Faker;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TrainsTableSeeder extends Seeder
